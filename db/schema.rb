affe6dceb42d56291f269ef1893bd90a3450597c
Charles Benard <charles1benard@gmail.com>
2017-05-23 20:01:01 +0200

    working on dashboard design

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523141549) do
+ActiveRecord::Schema.define(version: 20170523144753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
