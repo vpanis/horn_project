c5cbb662cd38e241eaaf718b1fd2f1c7293e1317
Vincent Panis <vincent@come2link.com>
2017-05-24 13:02:49 +0200

    skip login ok

--- app/controllers/horntrips_controller.rb
+++ app/controllers/horntrips_controller.rb
@@ -1,4 +1,5 @@
 class HorntripsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_horntrip, only: [:show, :edit, :update, :destroy]
 
   def index
