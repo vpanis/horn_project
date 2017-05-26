dc2a76ccbdc618f40f2a167dafec6a6dbcd9560a
Vincent Panis <vincent@come2link.com>
2017-05-26 15:11:59 +0200

    photo profil avatar large et photo de card avatar petit le tout rond

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     if current_user.photo.nil?
       current_user.facebook_picture_url || asset_path('mike_horn.png')
     else
-      cl_image_path(current_user.photo.path)
+      cl_image_path(current_user.photo.path, height: 100, width: 100, crop: :fill)
     end
   end
 end
